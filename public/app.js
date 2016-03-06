f24d8341c9ba1eb169b795e66e9081ca3cc49cbf
Woohyun Kim <r45r54r45@naver.com>
2016-03-06 16:37:54 +0900

    init

---
+++ public/app.js
@@ -0,0 +1,17 @@
+var app = angular.module("sampleApp", ["firebase"]);
+
+app.controller("SampleCtrl", function($scope, $firebaseArray) {
+  var ref = new Firebase("https://sizzling-inferno-3457.firebaseio.com/messages");
+
+  // create a synchronized array
+  $scope.messages = $firebaseArray(ref);
+
+  // add new items to the array
+  // the message is automatically added to our Firebase database!
+  $scope.addMessage = function() {
+    $scope.messages.$add({
+      text: $scope.newMessageText
+    });
+  };
+  // click on `index.html` above to see $remove() and $save() in action
+});
